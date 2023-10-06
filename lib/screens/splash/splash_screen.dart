c687995693cebfa9ecdbb5a56abe8d3646bcd617
theRealNonso <okoroaforkelechi123@gmail.com>
2023-10-06 12:55:40 +0000

    fixed splash screen widget

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -6,18 +6,23 @@ import '../../controllers/splash/splash_controller.dart';
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
+  Widget _buildLogoImage() {
+    return Center(
+      child: Image.asset(
+        'assets/jpg/safeChatLogo.jpg',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Get.put(SplashController());
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(
-            child: Image.asset(
-              'assets/jpg/safeChatLogo.jpg',
-            ),
-          )
+          _buildLogoImage(),
         ],
       ),
     );
