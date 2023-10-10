50952f05cae00568afb466389b50721aa2f08868
theRealNonso <okoroaforkelechi123@gmail.com>
2023-10-10 09:46:28 +0000

    done with create page, login, splash and added dio dependency

---
+++ lib/screens/info/info2_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:safe_chat/appConfig/manager/font_manager.dart';
+
+import '../../appConfig/manager/theme_manager.dart';
+
+class InfoTwoScreen extends StatelessWidget {
+  const InfoTwoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildSafeChatLogo(),
+                const SizedBox(height: 50),
+                _buildImage(),
+                const SizedBox(height: 100),
+                _buildButtons(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSafeChatLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Safe Chat',
+          style: TextStyle(fontSize: AppFontSize.s20),
+        ),
+        Image.asset(
+          'assets/jpg/safeChatWhiteLogo.jpg',
+          height: 40,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildImage() {
+    return Image.asset(
+      'assets/jpg/illustration.jpg',
+      height: 200,
+    );
+  }
+
+  Widget _buildButtons(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/login');
+          },
+          style: ElevatedButton.styleFrom(
+            foregroundColor: AppColors.activeButton,
+            backgroundColor: AppColors.white,
+            side: BorderSide(
+                color: AppColors.activeButton
+            )
+          ),
+          child: const Text('Login'),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/sign_up');
+          },
+          child: const Text('Sign Up'),
+        ),
+      ],
+    );
+  }
+}
