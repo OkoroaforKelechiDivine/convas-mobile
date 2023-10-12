53d54db90390bb5f06eabf1775b80455d60768d9
theRealNonso <okoroaforkelechi123@gmail.com>
2023-10-12 12:51:00 +0000

    added single child scroll

--- lib/screens/reset_password/set_reset_password.dart
+++ lib/screens/reset_password/set_reset_password.dart
@@ -16,51 +16,67 @@ class _ResetPasscodeScreenState extends State<ResetPasscodeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Padding(
-          padding: EdgeInsets.only(left: 50),
-          child: Text(
-            'Reset Password',
-            style: TextStyle(
-              fontSize: AppFontSize.s18,
-              fontWeight: AppFontWeight.light,
-            ),
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              _buildPasswordInputField(
+                controller: newPasswordController,
+                label: 'New Password',
+              ),
+              const SizedBox(height: 20),
+              _buildPasswordInputField(
+                controller: confirmPasswordController,
+                label: 'Confirm Password',
+              ),
+              const SizedBox(height: 20),
+              _buildResetPasswordButton(),
+            ],
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            TextField(
-              controller: newPasswordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'New Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: confirmPasswordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Confirm Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacementNamed('/login');
-              },
-              child: const Text('Reset Password'),
-            ),
-          ],
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: const Padding(
+        padding: EdgeInsets.only(left: 50),
+        child: Text(
+          'Reset Password',
+          style: TextStyle(
+            fontSize: AppFontSize.s18,
+            fontWeight: AppFontWeight.light,
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildPasswordInputField({
+    required TextEditingController controller,
+    required String label,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+
+  Widget _buildResetPasswordButton() {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).pushReplacementNamed('/login');
+      },
+      child: const Text('Reset Password'),
     );
   }
 
