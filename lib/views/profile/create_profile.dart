94479b4fca2f641931e0882812bf1de72d67871f
theRealNonso <okoroaforkelechi123@gmail.com>
2024-05-06 20:08:34 +0000

    done with create profile screen

--- lib/views/profile/create_profile.dart
+++ lib/views/profile/create_profile.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
-
-import '../../../theme_settings/manager/theme_manager.dart';
+import 'package:safe_chat/views/profile/widget/form.dart';
+import 'package:safe_chat/views/profile/widget/profile_picture.dart';
+import '../../theme_settings/manager/theme_manager.dart';
+import '../../view_models/create_profile/create_profile_view_model.dart';
 
 class ProfileScreen extends StatefulWidget {
-
   final String selectedGender;
 
   const ProfileScreen({Key? key, required this.selectedGender}) : super(key: key);
@@ -15,21 +14,20 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  File? _image;
-  String? userName;
-  String? firstName;
-  String? lastName;
-  DateTime? dateOfBirth;
-  String? emailAddress;
+  final ProfileScreenViewModel _viewModel = ProfileScreenViewModel();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.only(left: 50),
+          child: Text('Create Profile'),
+        ),
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back_ios_sharp),
           onPressed: () {
-            Navigator.of(context).pushNamed("/sign_up");
+            Navigator.of(context).pop();
           },
         ),
       ),
@@ -37,179 +35,26 @@ class _ProfileScreenState extends State<ProfileScreen> {
         child: Center(
           child: Column(
             children: <Widget>[
-              _buildProfilePicture(),
-              _buildProfileForm(),
+              const SizedBox(height: 20),
+              ProfilePictureWidget(selectedGender: widget.selectedGender, viewModel: _viewModel),
+              const SizedBox(height: 20),
+              ProfileFormWidget(viewModel: _viewModel),
             ],
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildProfilePicture() {
-    final isMale = widget.selectedGender == 'Male';
-    return GestureDetector(
-      onTap: _viewImage,
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: Stack(
-          children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundColor: AppColors.grey,
-              backgroundImage: _image != null ? FileImage(_image!) : isMale ? const AssetImage('assets/jpg/male-default-avatar.jpg') : const AssetImage('assets/jpg/female-default-avatar.jpg') as ImageProvider,
-            ),
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: GestureDetector(
-                onTap: _getImage,
-                child: Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.activeButton,
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  child: Icon(
-                    Icons.camera_alt,
-                    size: 20,
-                    color: AppColors.white,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-
-  Widget _buildProfileForm() {
-    final greenUnderline = UnderlineInputBorder(
-      borderSide: BorderSide(color: AppColors.green),
-    );
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Username',
-              enabledBorder: greenUnderline,
-              focusedBorder: greenUnderline,
-            ),
-            onChanged: (value) {
-              setState(() {
-                userName = value;
-              });
-            },
-          ),
-          GestureDetector(
-            onTap: () => _selectDate(context),
-            child: AbsorbPointer(
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Date of Birth',
-                  enabledBorder: greenUnderline,
-                  focusedBorder: greenUnderline,
-                ),
-                controller: TextEditingController(
-                    text: dateOfBirth != null
-                        ? "${dateOfBirth!.day}/${dateOfBirth!.month}/${dateOfBirth!.year}"
-                        : ""), // Display selected date
-              ),
-            ),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Bio',
-              enabledBorder: greenUnderline,
-              focusedBorder: greenUnderline,
-            ),
-            onChanged: (value) {
-              setState(() {
-                lastName = value;
-              });
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Hobbies',
-              enabledBorder: greenUnderline,
-              focusedBorder: greenUnderline,
-            ),
-            onChanged: (value) {
-              setState(() {
-                emailAddress = value;
-              });
-            },
-          ),
-          const SizedBox(height: 50),
-          ElevatedButton(
-            onPressed: _saveProfile,
-            child: const Text('Complete'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _getImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  void _viewImage() {
-    if (_image != null) {
-      Navigator.of(context).push(
-        MaterialPageRoute<void>(
-          builder: (context) {
-            return Scaffold(
-              appBar: AppBar(),
-              body: Center(
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: FileImage(_image!),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-            );
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('');
           },
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.blackColor
+          ),
+          child: Text('Save profile', style: TextStyle(color: AppColors.white)),
         ),
-      );
-    }
-  }
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? selectedDate = await showDatePicker(
-      context: context,
-      initialDate: dateOfBirth ?? DateTime.now(),
-      firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
+      ),
     );
-
-    if (selectedDate != null) {
-      setState(() {
-        dateOfBirth = selectedDate;
-      });
-    }
-  }
-
-  void _saveProfile() {
-    Navigator.pushNamed(context, "/home_page");
   }
 }
