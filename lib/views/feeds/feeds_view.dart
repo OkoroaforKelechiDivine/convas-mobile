e2b015030e18ce5e5a8cd01f7b678178792ff74f
theRealNonso <okoroaforkelechi123@gmail.com>
2024-05-15 14:27:45 +0000

    finished

--- lib/views/feeds/feeds_view.dart
+++ lib/views/feeds/feeds_view.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:safe_chat/app_style/manager/font_manager.dart';
-import 'package:safe_chat/locator/di.dart';
-import 'package:safe_chat/utilities/widgets/app_button.dart';
-import 'package:safe_chat/utilities/widgets/app_input_field.dart';
-import 'package:safe_chat/utilities/widgets/app_text.dart';
 import 'package:safe_chat/view_models/feeds/feeds_view_model.dart';
-import 'package:safe_chat/views/create_profile/create_profile_view.dart';
 import 'package:safe_chat/views/feeds/widget/app_bar.dart';
 import 'package:safe_chat/views/feeds/widget/button_text.dart';
 import 'package:safe_chat/views/feeds/widget/post_fields.dart';
+import 'package:safe_chat/views/feeds/widget/user_post_list.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../app_style/manager/theme_manager.dart';
+
 class FeedsView extends StatelessWidget {
   const FeedsView({Key? key}) : super(key: key);
 
@@ -97,91 +93,7 @@ class FeedsView extends StatelessWidget {
                   onSharePressed: () {},
                 ),
                 SizedBox(height: 20.h),
-                SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height - 250.h,
-                    child: ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: viewModel.users.length,
-                      itemBuilder: (context, index) {
-                        final user = viewModel.users[index];
-                        return ListTile(
-                          title: Row(
-                            children: [
-                              Row(
-                                children: [
-                                  GestureDetector(
-                                    onTap: () {
-                                      navigationService.push(const CreateProfileView(selectedGender: "selectedGender"));
-                                    },
-                                    child: Row(
-                                      children: [
-                                        CircleAvatar(
-                                          backgroundImage: AssetImage(user.image),
-                                        ),
-                                        AppText(
-                                          user.name,
-                                        ),
-                                      ],
-                                    ),
-
-                                  ),
-                                  SizedBox(width: 10.h),
-                                  AppText('${user.datePosted} ${user.timePosted}', fontSize: AppFontSize.s12, color: AppColors.grey),
-                                ],
-                              ),
-                            ],
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 4.h),
-                              // AppText(user.userPost),
-                              GestureDetector(
-                                onTap: () {
-                                  // Handle comments tap
-                                  viewModel.showComments(index);
-                                },
-                                child: Row(
-                                  children: [
-                                    const Icon(Icons.thumb_up),
-                                    SizedBox(width: 4.w),
-                                    AppText('${user.impressionsCount} impressions'),
-                                    SizedBox(width: 20.w),
-                                    const Icon(Icons.comment),
-                                    SizedBox(width: 4.w),
-                                    AppText('${user.commentsCount} comments'),
-                                  ],
-                                ),
-                              ),
-                              const SizedBox(height: 8),
-                              if (user.comments.isNotEmpty && viewModel.shouldShowComments(index))
-                                ...user.comments.map((comment) {
-                                  return Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(left: 8),
-                                        child: Row(
-                                          children: [
-                                            const Icon(Icons.comment),
-                                            SizedBox(width: 4.w),
-                                            Text('${comment.commenter}: ${comment.comment}'),
-                                          ],
-                                        ),
-                                      ),
-                                      SizedBox(height: 4.h),
-                                    ],
-                                  );
-                                }).toList(),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
+                UserListView(viewModel: viewModel),
               ],
             ),
           ),
@@ -190,3 +102,4 @@ class FeedsView extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/views/feeds/widget/user_post_list.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../app_style/manager/font_manager.dart';
+import '../../../app_style/manager/theme_manager.dart';
+import '../../../locator/locator.dart';
+import '../../../utilities/widgets/app_text.dart';
+import '../../../view_models/feeds/feeds_view_model.dart';
+import '../../create_profile/create_profile_view.dart';
+
+class UserListView extends StatelessWidget {
+  final FeedsViewModel viewModel;
+
+  const UserListView({Key? key, required this.viewModel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height - 250.h,
+        child: ListView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: viewModel.users.length,
+          itemBuilder: (context, index) {
+            final user = viewModel.users[index];
+            return ListTile(
+              title: Row(
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          navigationService.push(const CreateProfileView(selectedGender: "selectedGender"));
+                        },
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage(user.image),
+                            ),
+                            AppText(
+                              user.name,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10.h),
+                      AppText('${user.datePosted} ${user.timePosted}', fontSize: AppFontSize.s12, color: AppColors.grey),
+                    ],
+                  ),
+                ],
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 4.h),
+                  GestureDetector(
+                    onTap: () {
+                      viewModel.showComments(index);
+                    },
+                    child: Row(
+                      children: [
+                        const Icon(Icons.thumb_up),
+                        SizedBox(width: 4.w),
+                        AppText('${user.impressionsCount} impressions'),
+                        SizedBox(width: 20.w),
+                        const Icon(Icons.comment),
+                        SizedBox(width: 4.w),
+                        AppText('${user.commentsCount} comments'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  if (user.comments.isNotEmpty && viewModel.shouldShowComments(index))
+                    ...user.comments.map((comment) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Row(
+                              children: [
+                                const Icon(Icons.comment),
+                                SizedBox(width: 4.w),
+                                Text('${comment.commenter}: ${comment.comment}'),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 4.h),
+                        ],
+                      );
+                    }).toList(),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
